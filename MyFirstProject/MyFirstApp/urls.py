b60ae85957e6b83733079560e47d2810fd5e8f37
Yssf67 <yssf.a672@gmail.com>
2026-05-07 10:35:51 +0200

    maj 07/05

--- MyFirstProject/MyFirstApp/urls.py
+++ MyFirstProject/MyFirstApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
+path('index2/', views.index)
 ]
--- MyFirstProject/MyFirstApp/views.py
+++ MyFirstProject/MyFirstApp/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'MyFirstApp/index.html')
+    return render(request, 'MyFirstApp/index2.html')
 
